93cff9dacd7d6f11d39164a521be6efb73d3c60c
James McPherson <jemcphe@fullsail.edu>
2012-03-08 01:20:58 -0600

    Add PDF Flowchart
    Flowchart is prep for Project 3

--- main.js
+++ main.js
@@ -9,17 +9,17 @@
 window.addEventListener("DOMContentLoaded", function() {
 	
 	//getElementById function
-	function makeElement(x){
+	function $(x){
 		var theElement = document.getElementById(x);
 		return theElement;
 	};
 	
 	//Create select field element and populate with options.
 	function makeDropDown() {
-		var 	formTag = document.getElementsByTagName("form"); //formTag is an array of all the form tags.
-				selectLi = makeElement("select"),
+		var 	formTag = document.getElementsByTagName("form"), //formTag is an array of all the form tags.
+				selectLi = $("select"),
 				makeSelect = document.createElement("select");
-				makeSelect.setAttribute("id", "groups");
+				makeSelect.setAttribute("id", "position");
 		for(var i=0, j=positions.length; i<j; i++){
 			var makeOption = document.createElement("option");
 			var optText = positions[i];
@@ -27,19 +27,104 @@ window.addEventListener("DOMContentLoaded", function() {
 			makeOption.innerHTML = optText;
 			makeSelect.appendChild(makeOption);
 		}
-		selectLi.appendChild();
+		selectLi.appendChild(makeSelect);
 	};
 	
+	//Find value of selected radio button.
+	function getCheckboxValue(){
+		if($('starter').checked) {
+			starterValue = $("starter").value;
+		}else {
+			starterValue = "No";
+		}
+	}
+	
+	function toggleControls(n){
+		switch(n){
+			case"on":
+				$('addPlayerForm').style.display = "none";
+				$('clear').style.display = "inline";
+				$('display').style.display = "none";
+				$('addNew').style.display = "inline";
+				break;
+			case "off":
+				$('addPlayerForm').style.display = "block";
+				$('clear').style.display = "inline";
+				$('display').style.display = "inline";
+				$('addNew').style.display = "none";
+				$('items').style.display = "none";
+				break;
+			default:
+				return false;
+		}
+	}
+	
+	function storeData(){
+		var 	id									= Math.floor(Math.random()*1000001);
+		getCheckboxValue();
+		// gather up all our form field values and store in an object.
+		// Object properties contain array with the form label and input value.
+		var item = {};
+				item.position				= ["Position:", $('position').value];
+				item.pname				= ["Player Name:", $('pname').value];
+				item.bye						= ["Bye Week:", $('byeweek').value];
+				item.starter				= ["Starter:", starterValue];
+				item.skill						= ["Skill Level:", $('skill').value];
+				item.notes					= ["Notes:", $('notes').value];
+		//Save Data into Local Storage: Use Stringify to convert our object to a string.
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Player Saved!");
+	};
+	 
+	function getData() {
+		toggleControls("on");
+		if(localStorage.length === 0) {
+			alert("There are no players stored!")
+		}
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "players");
+		document.body.appendChild(makeDiv);
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		$('players').style.display = "block";
+		for(var i=0, j=localStorage.length; i<j; i++) {
+			var makeLi = document.createElement('li');
+			makeList.appendChild(makeLi);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			var obj = JSON.parse(value);
+			var makeSubList = document.createElement('ul');
+			makeLi.appendChild(makeSubList);
+			for(var n in obj) {
+				var makeSubLi = document.createElement('li');
+				makeSubList.appendChild(makeSubLi);
+				var optSubText = obj[n][0] +" "+ obj[n][1];
+				makeSubLi.innerHTML = optSubText;
+			}
+		}
+	}
+	
+	function clearLocal() {
+		if(localStorage.length === 0) {
+			alert("There is no data to clear.");
+		}else{
+			localStorage.clear();
+			alert("All Players have been deleted!");
+			window.location.reload();
+			return false;
+		}
+	}
 	
 	//variable defaults
-	var positions = ["--Select Position--", "QB", "RB", "WR", "TE", "K", "DEF"];
+	var 	positions = ["--Select Position--", "QB", "RB", "WR", "TE", "K", "DEF"],
+			starterValue = "No";
 	makeDropDown();
 	
 	//Set Link & submit Click Events
-	var displayLink = $('displayLink');
+	var displayLink = $('display');
 	displayLink.addEventListener("click", getData);
 	var clearLink = $('clear');
 	clearLink.addEventListener("click", clearLocal);
-	var save = $('submit');
-	save.addEventListener("click", storeData);
+	var saveLink = $("submit");
+	saveLink.addEventListener("click", storeData);
 });
